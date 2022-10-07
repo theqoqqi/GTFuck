4f527f7d3a765fac800c63b6933280c0777c7baa
Qoqqi <sazykin.georgii@gmail.com>
2022-10-07 19:59:25 +0300

    Аллах бабах

--- My/Scripts/AllahBabah.cs
+++ My/Scripts/AllahBabah.cs
@@ -1,5 +1,6 @@
 using System.Windows.Forms;
 using GTA;
+using GTA.Math;
 
 namespace My.Scripts {
     public class AllahBabah : Script {
@@ -10,18 +11,29 @@ namespace My.Scripts {
 
         void OnKeyDown(object sender, KeyEventArgs e) {
             if (e.KeyCode == Keys.NumPad9) {
-                Run();
+                Babah();
             }
         }
 
-        private void Run() {
-            Audio.PlaySoundAt(Finder.PlayerPed, "Monkey_Scream", "FBI_05_SOUNDS");
+        private void Babah() {
+            var peds = World.GetNearbyPeds(Finder.PlayerPed, 10);
+            var vehicles = World.GetNearbyVehicles(Finder.PlayerPed, 10);
             
-            Wait(3000);
+            BabahEntities(peds, 20, 5);
+            BabahEntities(vehicles, 15, 4);
+        }
+
+        private void BabahEntities<T>(T[] entities, float basePower, float verticalPower) where T : Entity {
+            var origin = Finder.PlayerPosition;
             
-            var radius = RandomUtils.NextFloat(10, 15);
-            
-            World.AddExplosion(Finder.PlayerPosition, ExplosionType.Barrel, radius, 0);
+            foreach (var entity in entities) {
+                var difference = entity.Position - origin;
+                var direction = difference.Normalized;
+                var power = basePower - difference.Length();
+                var force = direction * power + Vector3.WorldUp * verticalPower;
+
+                entity.ApplyForce(force);
+            }
         }
     }
 }
